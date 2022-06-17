e265b11d28727e3d7108b14e4359e36f9f5d75e2
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2022-06-17 14:30:48 +0530

    Next Permutation
    https://www.codingninjas.com/codestudio/problems/next-permutaion_893046?topList=striver-sde-sheet-problems

---
+++ Next Permutation.cpp
@@ -0,0 +1,22 @@
+vector<int> nextPermutation(vector<int> &permutation, int n)
+{
+    //  Write your code here.
+            int k, l;
+            for (k = n - 2; k >= 0; k--) {
+                if (permutation[k] < permutation[k + 1]) {
+                    break;
+                }
+            }
+            if (k < 0) {
+                reverse(permutation.begin(), permutation.end());
+            } else {
+                for (l = n - 1; l > k; l--) {
+                    if (permutation[l] > permutation[k]) {
+                        break;
+                    }
+                } 
+                swap(permutation[k], permutation[l]);
+                reverse(permutation.begin() + k + 1,permutation.end());
+            }
+    return permutation;
+    }
