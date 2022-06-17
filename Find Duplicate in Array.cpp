784363aed0019af307d9f0ebe8eaf43e1ca99168
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2022-06-18 01:09:34 +0530

    Find Duplicate in Array
    https://www.codingninjas.com/codestudio/problems/find-duplicate-in-array_1112602?topList=striver-sde-sheet-problems

---
+++ Find Duplicate in Array.cpp
@@ -0,0 +1,16 @@
+int findDuplicate(vector<int> &nums, int n){
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        
+        fast = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+}
