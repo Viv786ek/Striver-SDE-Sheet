d3d8711845f47c4349da96585e506480e30faa49
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2022-06-20 13:18:09 +0530

    Reverse Linked List
    https://www.codingninjas.com/codestudio/problems/reverse-the-singly-linked-list_799897?topList=striver-sde-sheet-problems

---
+++ Reverse Linked List.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
+{
+    LinkedListNode<int> *dummy=NULL;
+        while(head){
+            LinkedListNode<int> *next=head->next;
+            head->next=dummy;
+            dummy=head;
+            head=next;
+        }
+        return dummy;
+}
