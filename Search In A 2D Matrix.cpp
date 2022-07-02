0fdfcaacf99b7cd6db9968285d627042782e1e57
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2022-07-02 17:31:38 +0530

    Search In A 2D Matrix
    https://www.codingninjas.com/codestudio/problems/search-in-a-2d-matrix_980531?topList=striver-sde-sheet-problems

---
+++ Search In A 2D Matrix.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+#include <vector>
+
+bool findTargetInMatrix(vector < vector < int >> & mat, int m, int n, int target) {
+     int left = 0, right = m*n-1;
+    while(left != right) {
+        int mid = (left + (right - left)/2);
+        if(mat[mid/n][mid%n] < target)
+            left = mid+1;
+        else
+            right = mid;
+    }
+    return mat[right/n][right%n] == target;
+}
